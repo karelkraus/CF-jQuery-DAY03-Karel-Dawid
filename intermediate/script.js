365916126d7aa77f802043abb4aee4e0dd0ba7c1
dawidgostek <dawid.gostek@gmail.com>
2020-10-07 15:09:50 +0200

    css for intermediate

--- intermediate/script.js
+++ intermediate/script.js
@@ -1,8 +1,8 @@
 var storeData = JSON.parse(data)
 
 for (var i=0; i < storeData.length; i++) {
-	document.getElementById("content").innerHTML += `<div class=${storeData[i].type}> <img src=${storeData[i].img}> 
-	  <br> ${storeData[i].name} <br> <p id="${i}"> <span class="btnMinus">${storeData[i].quantity}</span><button class="minus">Add to cart</button></p> 
+	document.getElementById("content").innerHTML += `<div id="box" class=${storeData[i].type}> <img src=${storeData[i].img}> 
+	  <div class="box"><p class="text">${storeData[i].name}</p> <br> <p id="${i}" class="text2" ><button class="minus">Add to cart</button> <br> <span class="btnMinus">${storeData[i].quantity}</span></p></div> 
 	  </div> `;
 };
 
