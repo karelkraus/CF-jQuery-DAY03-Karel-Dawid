e0ad78a38052d12f7fd86717dcdf9beb62307500
Karel Kraus <kaja.kraus@gmail.com>
2020-10-07 16:14:56 +0200

    fixed cursor

--- advanced/myscript.js
+++ advanced/myscript.js
@@ -1,25 +1,12 @@
 $(document).bind('mousemove', function(e){
    $('#moveme').css({
-     top: e.pageY - $("#moveme").height()/2, // just minus by half the height
-     left:  e.pageX - $("#moveme").width()/2, // just minus by half the width
+     top: e.pageY - $("#moveme").height(), 
+     left:  e.pageX - $("#moveme").width(), 
    });
  });
 
-
 $(function() {
-$("#moveme").click(function(e) {
-    var o = {
-        left: e.pageX - 12.5,
-        top: e.pageY - 12.5            
-    };
-    $("#moveme").clone().appendTo('body').offset(o).fadeOut(3000).fadeIn(3000);
+$("#moveme").click(function() {
+    $("#moveme").clone().appendTo('body').fadeOut(3000).fadeIn(3000);
 });
 });
-
-/*let moveme = document.getElementById('moveme');
-const onMouseMove = (e) =>{
-  moveme.style.left = e.pageX + 'px';
-  moveme.style.top = e.pageY + 'px';
-}
-document.addEventListener('mousemove', onMouseMove);
-*/
